5bfe06e4ecbb7921eb2d705f051a36a51f6e6063
PautkinYegor <mr.tsarap@mail.ru>
2017-05-21 18:22:08 +0300

    Update third_task.js

--- JS/third_task.js
+++ JS/third_task.js
@@ -1,11 +1,11 @@
 console.log('cycle with for:');
 for (let i = 2; i <= 10; i += 2) {
-  console.log(i)
+  console.log(i);
 }
 
 console.log('cycle with while:');
-i = 2;
+let i = 2;
 while (i <= 10) {
   console.log(i);
-  i += 2
+  i += 2;
 }
