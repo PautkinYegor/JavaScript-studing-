b5823c86fd5a689f752d987f38a9ae7832889263
PautkinYegor <mr.tsarap@mail.ru>
2017-05-23 17:29:56 +0300

    Update second_task.js

--- JS/second_task.js
+++ JS/second_task.js
@@ -1,15 +1,27 @@
 let login = prompt('Please, enter in system:', 'login');
 let message = 'Вход отменён';
 
-if (login == null) {
+if (login == null)
+{
   alert(message);
-} else if (login == 'Админ') {
-    let pass = prompt('Enter the password:', 'passwd');
-    if (pass == null){
-      alert(message);
-    } else {
-      (pass == 'Чёрный Властелин') ? alert('Добро пожаловать!') : alert('Пароль неверен');
+} 
+else if (login == 'Админ') 
+{
+  pass = prompt('Enter the password:', 'passwd');
+  if (pass == null)
+  {
+    alert(message);
+  }
+  else if (pass == 'Чёрный Властелин')
+  {
+    alert('Добро пожаловать!');
+  }
+    else
+    {
+      alert('Пароль неверен');
     }
-} else {
-    alert('Я вас не знаю');
 }
+  else
+  {
+    alert('Я вас не знаю');
+  }
