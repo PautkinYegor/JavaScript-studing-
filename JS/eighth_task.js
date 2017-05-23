a4bff2114170bcce79e425db7e1bb787f324d5f1
PautkinYegor <mr.tsarap@mail.ru>
2017-05-23 22:51:12 +0300

    Create eighth_task.js

---
+++ JS/eighth_task.js
@@ -0,0 +1,15 @@
+let vasya = { name: "Вася", age: 23 };
+let masha = { name: "Маша", age: 18 };
+let vovochka = { name: "Вовочка", age: 6 };
+
+let people = [ vasya, masha, vovochka ];
+
+function compareAge(personA, personB) {
+    return personA.age - personB.age;
+}
+
+people.sort(compareAge);
+
+for (var i = 0; i < people.length; i++) {
+    console.log(people[i].name);
+}
