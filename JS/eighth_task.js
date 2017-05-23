6af157d5d5227aba04b5b1d9e0243911cc6714de
PautkinYegor <mr.tsarap@mail.ru>
2017-05-23 22:52:03 +0300

    Update eighth_task.js

--- JS/eighth_task.js
+++ JS/eighth_task.js
@@ -4,12 +4,14 @@ let vovochka = { name: "Вовочка", age: 6 };
 
 let people = [ vasya, masha, vovochka ];
 
-function compareAge(personA, personB) {
-    return personA.age - personB.age;
+function compareAge(personA, personB)
+{
+  return personA.age - personB.age;
 }
 
 people.sort(compareAge);
 
-for (var i = 0; i < people.length; i++) {
-    console.log(people[i].name);
+for (var i = 0; i < people.length; i++) 
+{
+  console.log(people[i].name);
 }
