4dfdd211ad02964173ffc77680713cf938785f6f
PautkinYegor <mr.tsarap@mail.ru>
2017-05-24 17:51:49 +0300

    Update canvas_cubes.js

--- canvas_cubes.js
+++ canvas_cubes.js
@@ -2,9 +2,11 @@ function createImage()
 { 
   let canvas = document.getElementById("canvas"); 
   let ctx = canvas.getContext("2d"); 
-  ctx.strokeRect(20, 20, 50, 50); 
-  ctx.fillRect(80, 80, 50, 50); 
-  ctx.clearRect(50, 50, 50, 50); 
+  ctx.strokeStyle = "red";
+  ctx.strokeRect(20,20,50,50);
+  ctx.fillStyle = "blue";
+  ctx.fillRect(80, 80, 50, 50);
+  ctx.clearRect(50,50,50,50);
 } 
 
 createImage();
