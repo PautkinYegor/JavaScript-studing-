9228e0939e2b1a1061db2211d71b6b771c706fc3
PautkinYegor <mr.tsarap@mail.ru>
2017-05-24 19:22:10 +0300

    Create canvas_smile.js

---
+++ canvas_smile.js
@@ -0,0 +1,19 @@
+
+function createImage()
+{
+  let canvas = document.getElementById("canvas");
+  let ctx = canvas.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(100, 100, 75, 2.2 * Math.PI, 0.8 * Math.PI, false);
+  ctx.lineWidth = 5;
+  ctx.strokeStyle = "blue";
+  ctx.stroke();
+  ctx.beginPath();
+  ctx.moveTo(80, 40);
+  ctx.lineTo(80, 120);
+  ctx.moveTo(120, 40);
+  ctx.lineTo(120, 120);
+  ctx.stroke();
+}
+
+createImage();
