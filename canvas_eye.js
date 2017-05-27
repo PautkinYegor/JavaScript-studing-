17942f423a0a902bebcd12e827a5059b3c10b342
PautkinYegor <mr.tsarap@mail.ru>
2017-05-27 16:34:17 +0300

    Update canvas_eye.js

--- canvas_eye.js
+++ canvas_eye.js
@@ -1,24 +1,25 @@
 //eye of lines
-//without reverse
+//with reverse
 function drawEye(startX, startY, revert)
 {
   let canvas = document.getElementById("canvas");
   let ctx = canvas.getContext("2d");
-  let width = 150;
+  let width = 250;
   startX = revert ? (startX + width) : startX;
   let coef = revert ? -1 : 1;
   ctx.lineWidth = 1;
   ctx.strokeStyle = "black";
   ctx.beginPath();
   ctx.moveTo(startX, startY);
-  ctx.bezierCurveTo(startX + 55, startY - 30, startX + 155, startY - 5, startX + 230, startY + 20);
+  ctx.bezierCurveTo(startX + 55 * coef, startY - 30, startX + 155 * coef, startY - 5, startX + 230 * coef, startY + 20);
   ctx.moveTo(startX, startY);
-  ctx.quadraticCurveTo(startX + 80, startY + 75, startX + 230, startY + 20);
-  ctx.moveTo(startX + 80, startY - 13);
-  ctx.quadraticCurveTo(startX + 55, startY + 20, startX + 80, startY + 40);
-  ctx.moveTo(startX + 160, startY - 1);
-  ctx.quadraticCurveTo(startX + 175, startY + 25, startX + 150, startY + 41);
+  ctx.quadraticCurveTo(startX + 80 * coef, startY + 75, startX + 230 * coef, startY + 20);
+  ctx.moveTo(startX + 80 * coef, startY - 13);
+  ctx.quadraticCurveTo(startX + 55 * coef, startY + 20, startX + 80 * coef, startY + 40);
+  ctx.moveTo(startX + 160 * coef, startY - 1);
+  ctx.quadraticCurveTo(startX + 175 * coef, startY + 25, startX + 150 * coef, startY + 41);
   ctx.stroke();
 }
 
 drawEye(30, 85, false);
+drawEye(280, 85, true);
