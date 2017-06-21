af8300831e26a4414c3dec2dc61c91131858a6e0
PautkinYegor <mr.tsarap@mail.ru>
2017-06-21 17:34:09 +0300

    Внесены некоторые корректировки из замечаний.

--- Canvas/mario/marioOneMore.js
+++ Canvas/mario/marioOneMore.js
@@ -1,103 +1,113 @@
 //9 добавить в drawSpriteMap поддержку отрисовки карты любой размерности из заданных координат
+// - избавиться от передачи completeCallback и загрузки спрайта в функции - drawSpriteMap, т.к. спрайт теперь общий и может грузиться только единовременно на старте
+// - рисовать каждый объект отдельно в заданных координатах, а не пытаться рисовать карту и объект в drawSpriteMap
+// + константные координаты тайлов в спрайтах именовать заглавными буквами
+// + не смешивать camelCase и snake_case в именовании переменных и методов
+// - разбить имеющиеся карты на отдельные объекты там, где это соотвествует здравому смыслу
+// + вместо тайлов неба можно изначально заливать кадр соотвествующим цветом
+// - function drawSpriteMap(spriteMap, offsetX, offsetY, canvasContext)
 
 //Размер ячейки на карте
 const CELL_SIZE = 16;
-const CS_WIDTH = 16;
-const CS_HEIGHT = 10;
+const WIDTH = 16;
+const HEIGHT = 10;
 const ZOOM_COEFF = 4;
 
 //повторяющиеся элименты
-const backgroundSky = {x:2.5, y:9.2};
-const empty = {x:0, y:0};
-const ground = {x:3.88, y:5.8};
-const coin = {x:4.22, y:14.3};
-const block_brick = {x:3.88, y:1};
-const block_iron = {x:3.88, y:2.12};
+const EMPTY = {x:0, y:0};
+const GROUND = {x:3.88, y:5.8};
+const BLOCK_BRICK = {x:3.88, y:1};
+const BLOCK_IRON = {x:3.88, y:2.12};
+const CLOUD1 = {x:4.12, y:23.5};
+const CLOUD2 = {x:5.12, y:23.5};
+const CLOUD3 = {x:6.12, y:23.5};
+const CLOUD4 = {x:7.12, y:23.5};
+const CLOUD5 = {x:4.12, y:24.5};
+const CLOUD6 = {x:5.12, y:24.5};
+const CLOUD7 = {x:6.12, y:24.5};
+const CLOUD8 = {x:7.12, y:24.5};
+const SHRUB11 = {x:1.08, y:19.15};
+const SHRUB12 = {x:2.08, y:19.15};
+const SHRUB21 = {x:7.28, y:19.15};
+const SHRUB22 = {x:8.28, y:19.15};
+const SHRUB23 = {x:9.28, y:19.15};
 
 //объекты
-const clouds = [
-  [{x:4.12, y:23.5}, {x:5.12, y:23.5}, {x:6.12, y:23.5}, {x:7.12, y:23.5}],
-  [{x:4.12, y:24.5}, {x:5.12, y:24.5}, {x:6.12, y:24.5}, {x:7.12, y:24.5}]
-];
-const mario = [
-  [{x:1.55, y:21.7}]
-];
+function clouds() {
+  return [
+    [CLOUD1, CLOUD2, CLOUD3, CLOUD4],
+    [CLOUD5, CLOUD6, CLOUD7, CLOUD8]
+  ];
+}
+function mario() {
+  return [
+    [{x:1.55, y:21.7}]
+  ];
+}
+function coin() {
+  return [
+    [{x:4.22, y:14.3}]
+  ];
+}
+function shrub1() {
+  return [
+    [SHRUB11, SHRUB12]
+  ];
+}
+function shrub2() {
+  return [
+    [SHRUB21, SHRUB22, SHRUB23]
+  ]
+}
 
 //maps
-const map_sky = [
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 1ый ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 2ой ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 3ий ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 4ый ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 5ый ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 6ой ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 7ой ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 8ой ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky],  // 9ый ряд
-  [backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky, backgroundSky]  // 10ый ряд
+const mapGround = [
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 1ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 2ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 3ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 4ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, BLOCK_BRICK, BLOCK_IRON, BLOCK_BRICK, BLOCK_IRON, BLOCK_BRICK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 5ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 6ый ряд
+  [EMPTY, EMPTY, BLOCK_BRICK, BLOCK_IRON, BLOCK_BRICK, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 7ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 8ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 9ый ряд
+  [GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND, GROUND]  // 10ый ряд
 ];
 
-const map_ground = [
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 1ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 2ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 3ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 4ый ряд
-  [empty, empty, empty, empty, empty, empty, block_brick, block_iron, block_brick, block_iron, block_brick, empty, empty, empty, empty, empty],  // 5ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 6ый ряд
-  [empty, empty, block_brick, block_iron, block_brick, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 7ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 8ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 9ый ряд
-  [ground, ground, ground, ground, ground, ground, ground, ground, ground, ground, ground, ground, ground, ground, ground, ground]  // 10ый ряд
-];
-
-const map_coins = [
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 1ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 2ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 3ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, coin, empty, coin, empty, empty, empty, empty, empty, empty],  // 4ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 5ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 6ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 7ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 8ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 9ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty]  // 10ый ряд
-];
-
-const map_shrubsAndClouds = [
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, {x:4.12, y:23.5}, {x:5.12, y:23.5}, {x:6.12, y:23.5}, {x:7.12, y:23.5}, empty, empty, empty],  // 1ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, {x:4.12, y:24.5}, {x:5.12, y:24.5}, {x:6.12, y:24.5}, {x:7.12, y:24.5}, empty, empty, empty],  // 2ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 3ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 4ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 5ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 6ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 7ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty],  // 8ый ряд
-  [empty, empty, empty, {x:1.08, y:19.15}, {x:2.08, y:19.15}, empty, empty, empty, empty, empty, empty, {x:7.28, y:19.15}, {x:8.28, y:19.15}, {x:9.28, y:19.15}, empty, empty],  // 9ый ряд
-  [empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty, empty]  // 10ый ряд
+const mapShrubsAndClouds = [
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, CLOUD1, CLOUD2, CLOUD3, CLOUD4, EMPTY, EMPTY, EMPTY],  // 1ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, CLOUD5, CLOUD6, CLOUD7, CLOUD8, EMPTY, EMPTY, EMPTY],  // 2ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 3ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 4ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 5ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 6ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 7ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],  // 8ый ряд
+  [EMPTY, EMPTY, EMPTY, SHRUB11, SHRUB12, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, SHRUB21, SHRUB22, SHRUB23, EMPTY, EMPTY],  // 9ый ряд
+  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]  // 10ый ряд
 ];
 
 function start() {
-  let example = document.getElementById("example");
-  ctx = example.getContext('2d');
+  let canvas = document.getElementById("marioScene");
+  ctx = canvas.getContext('2d');
+  canvas.style.backgroundColor = '#2f95ff';
 
   // Размер холста равный 8х8 клеток
-  example.width  = CS_WIDTH * CELL_SIZE * ZOOM_COEFF;
-  example.height = CS_HEIGHT * CELL_SIZE * ZOOM_COEFF;
+  canvas.width  = WIDTH * CELL_SIZE * ZOOM_COEFF;
+  canvas.height = HEIGHT * CELL_SIZE * ZOOM_COEFF;
 
   //раскомментируйте код,  если хотите посмотреть нарисованную сцену
-  drawSpriteMap(map_sky, ctx, false, 0, 0, 0, (ctx) => {
-    drawSpriteMap(map_ground, ctx, true, mario, 1, 8, (ctx) => {
-      drawSpriteMap(map_coins, ctx, false);
-      drawSpriteMap(map_shrubsAndClouds, ctx, true, clouds, 4, 0);
+    drawSpriteMap(mapGround, ctx, true, mario(), 1, 8, (ctx) => {
+      drawSpriteMap(mapShrubsAndClouds, ctx, true, clouds(), 4, 0);
     });
-  });
 }
 
 function calculate(value) {
   return (CELL_SIZE * ZOOM_COEFF * value);
 }
 
-function drawSpriteMap(map, ctx, supplement, object, mapX, mapY, completeCallback) { // object, mapX, mapY,
+function drawSpriteMap(map, ctx, supplement, object, mapX, mapY, completeCallback) {
+  // - function drawSpriteMap(spriteMap, offsetX, offsetY, canvasContext)
   const pic = new Image();
   pic.onload = () => {
 
