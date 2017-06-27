4afe8eea8ddaeb334e309ce226dfc8f9637d9589
PautkinYegor <mr.tsarap@mail.ru>
2017-06-27 12:39:17 +0300

    описаны общие идеи

--- Canvas/15/15my/puzzle15.js
+++ Canvas/15/15my/puzzle15.js
@@ -1,70 +1,73 @@
 let canvas = document.getElementById("puzzle15");
 let ctx = canvas.getContext("2d");
+canvas.style.backgroundColor = 'black';
 canvas.width = 480;
 canvas.height = 480;
-let cellSize = canvas.width / 4;
+let cellSize = 119;
 let x0 = Math.floor(Math.random() * 4); //x0 and y0 - coordinates of empty cube.
 let y0 = Math.floor(Math.random() * 4);
 let x = 0;
 let y = 0;
-let arrayOfNumbers = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 0]];
 
-function box() {
-  ctx.fillStyle = "black";
-  ctx.fillRect(1, 1, 480, 480);
-}
 
-// function nullCube() {  //возвращает координату пустой клетки
-//   for (let x1 = 0; x1 < 4; x1++) {
-//     for (let y1 = 0; y1 < 4; y1++) {
-//       if (arrayOfNumbers[x1][y1] === 0) {
-//         return{"x0":x1,"y0":y1};
-//       }
-//     }
-//   }
+// function createPlate(num) {
+// 	return {
+// 		value: num,
+// 		isSelected: false,
+// 	}
 // }
+// let arrayOfNumbers = [
+//     [createPlate(1), createPlate(2), createPlate(3), createPlate(4)],
+//     [createPlate(5), createPlate(6), createPlate(7), createPlate(8)],
+//     [createPlate(9), createPlate(10), createPlate(11), createPlate(12)],
+//     [createPlate(13), createPlate(14), createPlate(15), createPlate(0)],
+// ];
+//
+// function convertToGameFieldCoords(x, y) {} //returns {x: ..., y: ...} - относительнеы координаты в игровом поле;
+// function convertToPlateIndex(x, y) {} //returns {row: ..., column: ...} - индексы в игровом массиве;
+//
+// canvas.onclick = (event) {
+// 	const coords = convertToGameFieldCoords(event.layerX, event.layerY);
+// 	const plateIndex = convertToPlateIndex(coords.x, coords.y);
+// 	processMove(plateIndex.row, plateIndex.column);
+// }
+//
+// canvas.onmousemove = (event) {
+// 	const coords = convertToGameFieldCoords(event.layerX, event.layerY);
+// 	const plateIndex = convertToPlateIndex(coords.x, coords.y);
+//
+// 	hideSelection() //выключает выделение у всех элементов
+// 	select(plateIndex.row, plateIndex.column); //включает выделение у элемента
+// }
+//
+// requestAnimationFrame( ... )
 
-function drawCube() {
-  let numbers = 1;
+function drawSquare() {
+  canvas.onmousemove = (event) => {
+    console.log(event.pageX + ':' + event.pageY);
+  };
+  // let numbers = 1;
   for (y = 0; y < 4; y ++) {
     for (x = 0; x < 4; x ++) {
       if ((x !== x0) || (y !== y0)) {
+        const sizeOfSquare = 116;
         ctx.fillStyle = "silver";
-        ctx.fillRect(4 + x*119, 4 + y*119, 116, 116);
-        ctx.fillStyle = "black";
-        ctx.font = "italic 40pt Arial";
-        if (numbers >= 10) {
-          ctx.fillText(''+numbers, 29 + x*119, 74 + y*119, 66);
-        } else {
-          ctx.fillText(''+numbers, 49 + x*119, 74 + y*119, 66);
-        }
-        numbers++;
+        ctx.fillRect(4 + x * (sizeOfSquare + 3), 3 + y * (sizeOfSquare + 3), sizeOfSquare, sizeOfSquare);
+        // ctx.fillStyle = "black";
+        // ctx.font = "italic 40pt Times New Roman";
+        // const fontWidth = 60;
+        // const fontForY = 80 + y * sizeOfSquare;
+        // const fontForX1 = 40 + x * sizeOfSquare;
+        // const fontForX2 = 50 + x * sizeOfSquare;
+        // if (numbers >= 10) {
+        //   ctx.fillText('' + numbers, fontForX1, fontForY, fontWidth);
+        // } else {
+        //   ctx.fillText('' + numbers, fontForX2, fontForY, fontWidth);
+        // }
+        // numbers++;
       }
     }
   }
 }
 
-function move(x, y) {
-  console.log(x0, y0, x, y);
-  if (((x - 1 === x0 || x + 1 === x0) && y === y0) || ((y - 1 === y0 || y + 1 === y0) && x === x0)) {
-    let x1 = x;
-    let y1 = y;
-
-    //arrayOfNumbers[y][x] = arrayOfNumbers[y0][x0];
-    x0 = x1; //координаты пустой клетки стали координатами перемещаемой клетки
-    y0 = y1;
-    console.log(arrayOfNumbers[y][x]);
-    console.log(x0, y0);
-  }
-}
-
-canvas.onclick = function() {
-  let xx = (arrayOfNumbers.pageX - canvas.offsetLeft) / cellSize | 0;  // offsetLeft ?..подумать на счёт этого.(видимо, берёт координаты слева)
-  let yy = (arrayOfNumbers.pageY - canvas.offsetTop)  / cellSize | 0;
-  move(xx, yy);
-  drawCube();
-  console.log('click');
-};
-
-box();
-drawCube();
+drawSquare();
